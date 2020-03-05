10c419458c057452a5558b3dc022aead11cce5d6
Ben Sharp <bfwsharp@gmail.com>
2020-03-05 13:22:00 +0000

    Add intro fade

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-var tl = gsap.timeline();
-// t1.to(".centre", {opacity: 1, duration: 1});
-// t1.to(".info-text", {opacity: 1, duration: 1});
+document.addEventListener("DOMContentLoaded", function(){
+
+});
